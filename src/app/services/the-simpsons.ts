d5c422bed76eaf687d0b745917e3af5c3ba85bf4
Rafael Arango <rafa131842@gmail.com>
2026-03-01 16:44:24 -0500

    Comit 2

--- src/app/services/the-simpsons.ts
+++ src/app/services/the-simpsons.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class TheSimpsons {
-  
+@Injectable({ providedIn: 'root' })
+export class SimpsonsService {
+private baseURL = 'https://thesimpsonsapi.com/api';
+
+
+constructor(private http: HttpClient) {}
+getCharacters(page: number = 1): Observable<any> {
+return this.http.get(`${this.baseURL}/character/?page=${page}`);
+
+}
+getCharacterDetails(id: number): Observable<any> {
+return this.http.get(`${this.baseURL}/character/${id}`);
+}
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonInfiniteScroll, IonInfiniteScrollContent, IonSearchbar } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {SimpsonsService} from '../services/the-simpsons';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, CommonModule, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  characters: any[] = [];
+  currentPage = 1;
+  constructor(private rmService: SimpsonsService) { }
+  ngOnInit() {
+    this.loadCharacters();
+  }
+  loadCharacters(event?: any) {
+    this.rmService.getCharacters(this.currentPage).subscribe(res => {
+      this.characters.push(...res.results);
+      if (event) event.target.complete();
+    });
+  }
+  loadMore(event: any) {
+    this.currentPage++;
+    this.loadCharacters(event);
+  }
 }
